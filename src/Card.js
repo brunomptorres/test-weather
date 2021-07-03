5775491187f026bb2c581edf37023222d7a2a947
Bruno Torres <brunomptorres@Brunos-MacBook-Pro-5.local>
2021-07-03 23:00:04 +0100

    first commit

---
+++ src/Card.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+export default function Card() {
+  return <Button />;
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Form from "./Form";
+import Card from "./Card";
+function Index() {
+  return (
+    <div className="Index">
+      <Form />
+      <Card />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Index />, rootElement);
